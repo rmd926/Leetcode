23a37332f7e79c9218a62637ca9c6f83e2aa7546
Barry <75135224+rmd926@users.noreply.github.com>
2026-01-16 03:08:37 +0800

    Create solution.py

---
+++ 136. Single Number (Easy)/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        lookup = {}
+        for num in nums:
+            if num not in lookup:
+                lookup[num] = 1
+            else:
+                lookup[num] += 1
+        
+        for key, value in lookup.items():
+            if value == 1:
+                return key
