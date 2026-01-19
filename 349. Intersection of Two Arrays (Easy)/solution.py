666fe1cdadff9f1f16a3221554ca7f5a944034a7
Barry <75135224+rmd926@users.noreply.github.com>
2026-01-19 20:48:34 +0800

    Create solution.py

---
+++ 349. Intersection of Two Arrays (Easy)/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1 = set(nums1)
+        nums2 = set(nums2)
+        ans = []
+        for num in nums1:
+            if num in nums2:
+                ans.append(num)
+
+        return ans
+
+
+# Runtime: 0 ms Beats 100.00 %
+# Memory: 19.36 MB Beats 18.13 %
