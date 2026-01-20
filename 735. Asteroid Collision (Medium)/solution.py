1348b2dac113323a650da402cc22b5c8dd3f8912
Barry <75135224+rmd926@users.noreply.github.com>
2026-01-21 03:31:45 +0800

    Update solution.py

--- 735. Asteroid Collision (Medium)/solution.py
+++ 735. Asteroid Collision (Medium)/solution.py
@@ -1,19 +1,21 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         stack = []
-        for ast in asteroids:
-            if ast > 0:
-                stack.append(ast)
+        for vol in asteroids:
+            if vol > 0:
+                stack.append(vol)
             else:
                 # 當前最後一個 > 0，而且他比往負方向的體積小的時候，要pop掉
-                while stack and stack[-1] > 0 and stack[-1] < -ast:
+                while stack and stack[-1] > 0 and stack[-1] < abs(vol):
                     stack.pop()
-                if stack and stack[-1] == -ast:
+                # 如果stack還有東西且最後一個值恰好 和反方向的體積相同，兩者會一起爆炸 (僅需要pop掉在原本stack的最後一個)
+                if stack and stack[-1] == abs(vol):
                     stack.pop()
+                # 如果stack is empty 或者是最後一個元素也 < 0，那就把他append到stack裡面
                 elif not stack or stack[-1] < 0:
-                    stack.append(ast)
-        
+                    stack.append(vol)
+
         return stack
 
-# Runtime: 5 ms Beats 71.08 %
-# Memory: 20.44 MB Beats 8.91 %        
+# Runtime: 3 ms Beats 94.65 %
+# Memory: 20.40 MB Beats 17.79 %        
