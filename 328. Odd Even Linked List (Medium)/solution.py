845899ef43d7de3a81ce48a0e5727c43ec1c052f
Barry <75135224+rmd926@users.noreply.github.com>
2026-02-03 05:18:42 +0800

    Create solution.py

---
+++ 328. Odd Even Linked List (Medium)/solution.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+        
+        odd = head
+        even = head.next
+        even_head = even
+
+        while even and even.next:
+            odd.next = even.next
+            odd = even.next
+            even.next = odd.next
+            even = odd.next
+        
+        odd.next = even_head
+        return head
+
+# Runtime: 0 ms Beats 100.00 %
+# Memory: 21.10 MB Beats 47.20 %
