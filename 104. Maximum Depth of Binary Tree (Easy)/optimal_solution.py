74356622ec2a4772b77a0a4ec25262b2ba8686cb
Barry <75135224+rmd926@users.noreply.github.com>
2026-02-07 17:55:47 +0800

    Create optimal_solution.py

---
+++ 104. Maximum Depth of Binary Tree (Easy)/optimal_solution.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        return 1 + max(self.maxDepth(root.left), )
+            
+# Runtime: 0 ms Beats 100.00 %
+# Memory: 20.40 MB Beats 14.76 %
