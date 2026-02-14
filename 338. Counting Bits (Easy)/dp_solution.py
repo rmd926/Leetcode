c30cc49a99d800fe55c9132acb2d5d3ee9099ba3
Barry <75135224+rmd926@users.noreply.github.com>
2026-02-14 16:35:46 +0800

    Create dp_solution.py

---
+++ 338. Counting Bits (Easy)/dp_solution.py
@@ -0,0 +1,12 @@
+# dynamic programming method
+from typing import List
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        bits = [0] * (n + 1)
+        for i in range(1, n + 1):
+            bits[i] = bits[i >> 1] + (i & 1)
+        return bits
+
+# Runtime: 3 ms Beats 95.58 %
+# Memory: 20.17 % Beats 61.78 %
