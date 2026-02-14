5d4d883841097406139b26b789de606fc2445a0a
Barry <75135224+rmd926@users.noreply.github.com>
2026-02-14 16:27:36 +0800

    Create brute-force solution.py

---
+++ 338. Counting Bits (Easy)/brute-force solution.py
@@ -0,0 +1,18 @@
+# Brute-Force using bin tools
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = []
+        
+        for num in range(0, n+1):
+            temp = (bin(num)[2:])
+            count = 0
+            for char in temp:
+                if char == "1":
+                    count += 1
+                    
+            ans.append(count)
+
+        return ans
+
+# Runtime: 38 ms Beats 20.90 %
+# Memory: 20.32 % Beats 29.66 %
