7f13f896e4a77da25e923df44b8b3d568674439b
Barry <75135224+rmd926@users.noreply.github.com>
2026-02-21 16:52:22 +0800

    Create optimal_solution.py

---
+++ 540. Single Element in a Sorted Array (Medium)/optimal_solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        left, right = 0, len(nums)-1
+        while left < right:
+            mid = left + (right - left) // 2
+
+            if mid % 2 != 0:
+                mid -= 1
+
+            if nums[mid] == nums[mid+1]:
+                left = mid + 2
+            
+            else:
+                right = mid
+        
+        return nums[left]
+
+# Runtime: 0 ms Beats 100.00 %
+# Memory: 27.19 MB Beats 23.33 %
